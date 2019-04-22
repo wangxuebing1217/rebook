6a1dbf3cbf7941d5ead1d328554a1ad08ef513c4
wangxuebing1217 <gene.king941217@gmail.com>
2019-04-22 20:49:26 +0800

    二手书商城

---
+++ rebook-parent/rebook-common/src/main/java/pri/xy/rebook/App.java
@@ -0,0 +1,13 @@
+package pri.xy.rebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
